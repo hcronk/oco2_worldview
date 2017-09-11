5431f8a8df40e028a61e043cde6d3dd1719a6ee6
Heather Cronk <heather.cronk@colostate.edu>
2017-09-11 09:58:48 -0600

    Added check and filter for missing vertex geolocation. Added baseline plot capability

--- xco2_resolution_comparisons.py
+++ xco2_resolution_comparisons.py
@@ -6,36 +6,49 @@ import os
 import sys
 from shapely.geometry import Polygon, Point, LineString
 import matplotlib.pyplot as plt
+import json
+from glob import glob
 
-resolution = "1km"
+resolution = "500m"
 oco2_file_dir = "/home/codell/OCO2_results/b70/lite_B7Br"
 out_data_dir = "/home/hcronk/worldview/data"
 out_plot_dir = "/home/hcronk/worldview/plots"
 
-##Detroit
-#date = "2015-03-24"
-#geo_upper_left = [42.8,-83.4]
-#geo_lower_right = [41.2,-82.5]
+#Detroit
+date = "2015-03-24"
+geo_upper_left = [42.8,-83.4]
+geo_lower_right = [41.2,-82.5]
+
+plot_name = "Detroit_" + resolution + ".png"
+out_data_name = "Detroit_" + resolution + ".h5"
+
+baseline_plot_name = "Detroit_baseline.png"
+baseline_data_name = "Detroit_baseline.h5"
+
+oco2_file = "oco2_LtCO2_150324_B7305Br_160712115928s.nc4"
+
+#---#
+
+##Act America Pennsylvania
+#date = "2016-07-27"
+#geo_upper_left = [43.5, -78.9]
+#geo_lower_right = [39.6, -77.5]
 #
-#plot_name = "Detroit_vistool_" + resolution + ".png"
-#out_data_name = "Detroit_vistool_" + resolution + ".h5"
+#plot_name = "AAPenn_" + resolution + ".png"
+#out_data_name = "AAPenn_" + resolution + ".h5"
 #
-#oco2_file = "oco2_LtCO2_150324_B7305Br_160712115928s.nc4"
-
-#Act America Pennsylvania
-date = "2016-07-27"
-geo_upper_left = [43.5, -78.9]
-geo_lower_right = [39.6, -77.5]
-
-plot_name = "AAPenn_vistool_" + resolution + ".png"
-out_data_name = "AAPenn_vistool_" + resolution + ".h5"
-
-oco2_file = "oco2_LtCO2_160727_B7305Br_160923172049s.nc4"
+#baseline_plot_name = "AAPenn_baseline.png"
+#baseline_data_name = "AAPenn_baseline.h5"
+#
+#oco2_file = "oco2_LtCO2_160727_B7305Br_160923172049s.nc4"
 
 #----#
 
 out_plot = os.path.join(out_plot_dir, plot_name)
 out_data = os.path.join(out_data_dir, out_data_name)
+
+baseline_plot = os.path.join(out_plot_dir, baseline_plot_name)
+baseline_data = os.path.join(out_data_dir, baseline_data_name)
 
 #These numbers came from the GIBS ICD
 gibs_resolution_dict = {"2km" : 0.017578125, "1km" : 0.0087890625, "500m" : 0.00439453125, "250m" : 0.002197265625}
@@ -60,10 +73,10 @@ grid_y_elem = int(180 / gibs_resolution_dict[resolution])
 
 grid = np.empty([grid_x_elem, grid_y_elem], dtype=np.object)
 
-print os.path.join(oco2_file_dir, oco2_file)
+oco2_file_path = os.path.join(oco2_file_dir, oco2_file)
 
 #Read in data
-lf = LiteCO2File(os.path.join(oco2_file_dir, oco2_file))
+lf = LiteCO2File(oco2_file_path)
 lf.open_file()
 latitude = lf.get_lat()
 longitude = lf.get_lon()
@@ -81,11 +94,33 @@ vertex_latitude = np.squeeze(vertex_latitude[quality_mask, :])
 vertex_longitude = np.squeeze(vertex_longitude[quality_mask, :])
 data = data[quality_mask]
 
+if not glob(baseline_plot):
+    do_modis_overlay_plot(geo_upper_left,
+                          geo_lower_right, 
+		          date, vertex_latitude, vertex_longitude, data, var_lims = [395, 408],
+                          out_plot = baseline_plot, out_data = baseline_data)
+
+#Cut out data where any of the lat/lon vertices are missing
+vertex_miss_mask = np.where(np.logical_not(np.any(vertex_latitude == -999999, axis=1), np.any(vertex_longitude == -999999, axis=1)))
+if len(vertex_miss_mask[0]) != len(data): 
+    with open("good_quality_missing_vertex_geolocation.json", "r") as qc_file:
+        try: 
+            records = json.load(qc_file)
+        except:
+            records = {}
+    if oco2_file_path not in records.keys():
+        records[oco2_file_path] = list(np.where(np.logical_and(np.any(vertex_latitude == -999999, axis=1), np.any(vertex_longitude == -999999, axis=1)))[0])
+        with open("good_quality_missing_vertex_geolocation.json", "w") as qc_file:
+            json.dump(records, qc_file, indent=4)       
+    latitude = latitude[vertex_miss_mask]
+    longitude = longitude[vertex_miss_mask]
+    vertex_latitude = np.squeeze(vertex_latitude[vertex_miss_mask, :])
+    vertex_longitude = np.squeeze(vertex_longitude[vertex_miss_mask, :])
+    data = data[vertex_miss_mask]
+
 #Create lat/lon corner pairs from vertices
 #Each element of this array is a 4x2 array of lat/lon points of the parallelogram corners (Order: LL, UL, UR, LR)
 poly = np.dstack([vertex_latitude, vertex_longitude])
-print poly.shape
-#sys.exit()
 
 vlat_mins = vertex_latitude.min(axis=1)
 vlat_maxes = vertex_latitude.max(axis=1)
@@ -115,17 +150,17 @@ for n, vertices in enumerate(poly):
     
     for ll in zip_it:
         pt = Point(ll[0], ll[1])
-        x = np.where(ll[1] == lon_centers)[0][0]
-        y = np.where(ll[0] == lat_centers)[0][0]
         if pt.within(pg):
+            x = np.where(ll[1] == lon_centers)[0][0]
+            y = np.where(ll[0] == lat_centers)[0][0]
             if grid[x,y] is None:
                 grid[x,y] = [data[n]]
             else:
                 grid[x,y].append(data[n])
         else:
             if pg.exterior.distance(pt) <= 1e-3:
-                #x = np.where(ll[1] == lon_centers)[0][0]
-                #y = np.where(ll[0] == lat_centers)[0][0]
+                x = np.where(ll[1] == lon_centers)[0][0]
+                y = np.where(ll[0] == lat_centers)[0][0]
                 if grid[x,y] is None:
                     grid[x,y] = [data[n]]
                 else:
