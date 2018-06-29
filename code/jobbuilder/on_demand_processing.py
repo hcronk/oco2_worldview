fefa8cfaef30a63a5cd23c9abfe28949638d945c
Heather Cronk <heather.cronk@colostate.edu>
2018-06-29 08:55:01 -0600

    First pass at on_demand_processig. Functional but incomplete

--- code/jobbuilder/on_demand_processing.py
+++ code/jobbuilder/on_demand_processing.py
@@ -2,6 +2,34 @@ import os
 import sys
 import argparse
 from glob import glob
+import operator
+import re
+from routine_processing import get_image_filename, check_processing_or_problem, build_config
+
+#Global Variables
+data_dict = { "LtCO2" : 
+                { "xco2" : {"data_field_name" : "xco2", "preprocessing" : False, "range": [380, 430], "cmap" : "jet", "quality_info" : {"quality_field_name" : "xco2_quality_flag", "qc_val" :  0, "qc_operator" : operator.eq }}, 
+                  "xco2_relative" : {"data_field_name" : None, "preprocessing" : "ftp://aftp.cmdl.noaa.gov/products/trends/co2/co2_trend_gl.txt", "range": [-6, 6], "cmap" : "RdYlBu_r", "quality_info" : {"quality_field_name" : "xco2_quality_flag", "qc_val" :  0, "qc_operator" : operator.eq }}, 
+                  "tcwv" : {"data_field_name" : "Retrieval/tcwv", "preprocessing" : False, "range": [0, 75], "cmap" : "viridis", "quality_info" : {}}, 
+                },
+             "LtSIF" : 
+                { "sif757" : {"data_field_name" : "SIF_757nm", "preprocessing" : False, "range": [0, 2], "cmap" : "jet", "quality_info" : {}}, 
+                  "sif771" : {"data_field_name" : "SIF_771nm", "preprocessing" : False, "range": [0, 2], "cmap" : "jet", "quality_info" : {}}, 
+                  "sif_blended" : {"data_field_name" : None, "preprocessing" : True, "range": [0, 2], "cmap" : "jet", "quality_info" : {}}
+                }
+            }
+            
+tile_dict = { "NE": {"extent_box" : [0, 180, 0, 90]
+                    },
+              "SE": {"extent_box" : [0, 180, -90, 0]
+                    },
+              "SW": {"extent_box" : [-180, 0, -90, 0]
+                    },
+              "NW": {"extent_box" : [-180, 0, 0, 90]
+                    }
+            }
+
+extent_box = []
 
 if __name__ == "__main__":
     
@@ -10,36 +38,39 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Flags", prefix_chars='@')
     parser.add_argument("@v", "@@verbose", help="Prints some basic information during code execution", action="store_true")
     parser.add_argument("@f", "@@file", help="Full path to input file name for command line use", default=None)
-    parser.add_argument("@c", "@@config", help="Full path to text file containing a list of files to process, one filename per line", default=None)
-    parser.add_argument("@g", "@@geolocation", help="Custom geolocation box for testing and case study purposes. Fmt: [lat_min, lat_max, lon_min, lon_max]", nargs = '+', default=[])
+    parser.add_argument("@l", "@@file_list", help="Full path to text file containing a list of files to process, one filename per line", default=None)
+    parser.add_argument("@g", "@@geolocation", help="Lat/Lon extent box to plot. Fmt: [lat_min, lat_max, lon_min, lon_max]", nargs = '+', default=[])
     parser.add_argument("@r", "@@rgb", help="Overlay plots on MODIS RGB for testing and case study purposes", default=os.path.join(code_dir, "GIBS_Aqua_MODIS_truecolor.xml"))
     parser.add_argument("@o", "@@output_dir", help="Output directory for plots", default=os.path.join(code_dir, "plots"))
     parser.add_argument("@a", "@@vars", help="Variables to plot", nargs = '+', default=[])
+    parser.add_argument("@w", "@@overwrite", help="Overwrite existing plots", action="store_true")
     args = parser.parse_args()
+    
     verbose = args.verbose
     lite_file = args.file
-    config_file = args.config
+    file_list = args.file_list
     custom_geo_box = args.geolocation
     rgb = args.rgb
     out_plot_dir = args.output_dir
     user_defined_var_list = args.vars
+    overwrite = args.overwrite
     
     if user_defined_var_list:
-        user_defined_var_list = [x.strip("[,]") for x in user_defined_var_list]
+        user_defined_var_list = [x.strip("[,]") for x in user_defined_var_list]      
     
     if not os.path.exists(out_plot_dir):
         os.makedirs(out_plot_dir)
         
-        if not lite_file and not config_file or lite_file and config_file:
+    if not lite_file and not file_list or lite_file and file_list:
         print("Please provide a single file to process with the @f flag OR a file containing a list of files to process with the @c flag.")
         print("Exiting.")
         sys.exit()
     
-    if config_file:
-        if glob(config_file):
-            files = list(np.genfromtxt(config_file, dtype="str", delimiter="\n"))
+    if file_list:
+        if glob(file_list):
+            files = list(np.genfromtxt(file_list, dtype="str", delimiter="\n"))
         else:
-            print(config_file + " DNE. Exiting.")
+            print(file_list + " DNE. Exiting.")
             sys.exit()
     
     if lite_file:
@@ -51,46 +82,76 @@ if __name__ == "__main__":
     
     if custom_geo_box:
         custom_geo_box = [float(x.strip("[,]")) for x in custom_geo_box]
-        if len(custom_geo_box) != 4:
-            print("Custom geolocation box format: [lat_min, lat_max, lon_min, lon_max]")
-            print("Exiting.")
-            sys.exit()
-#Stuff moved from main code that should probably end up here            
-    if user_defined_var_list:
-        var_list = user_defined_var_list
-    else:
-        var_list = data_dict[product].keys()
-        
-    
-    
-    for var in var_list:
+        if len(custom_geo_box) != 4 \
+            or custom_geo_box[0] > custom_geo_box[1] \
+            or custom_geo_box[2] > custom_geo_box[3] \
+            or abs(custom_geo_box[0]) > 90. \
+            or abs(custom_geo_box[1]) > 90. \
+            or abs(custom_geo_box[2]) > 180. \
+            or abs(custom_geo_box[3]) > 180.:
+               print("Geolocation box format: [lat_min, lat_max, lon_min, lon_max] with Lat +/- 90 and Lon +/- 180")
+               print("Exiting.")
+               sys.exit()
+        lon_ul = custom_geo_box[2]
+        lon_lr = custom_geo_box[3]
+        lat_lr = custom_geo_box[0]
+        lat_ul = custom_geo_box[1]
+        extent_box = [lon_ul, lon_lr, lat_lr, lat_ul]
+
+    for lf in files:
         if verbose:
-            print("Processing "+ var)
-        if var not in data_dict[product].keys():
-            print(var + " is not defined in the " + product + " data dictionary. Please add it or check spelling.")
-            print("Exiting.")
-            sys.exit()
-    
-    
-    if not overwrite:
-        #double check there's something to do
-        if custom_geo_box:
-            for var in var_list:
-                if glob(os.path.join(out_plot_dir, var + "_Lat" + str(custom_geo_box[0]) + "to" + str(custom_geo_box[1]) + "_Lon" + str(custom_geo_box[2]) + "to" + str(custom_geo_box[3]) + "_" + global_plot_name_tags)):
-                    var_list.remove(var)
+            print("Processing " + lite_file)  
+        
+        lite_file_basename = os.path.basename(lf)
+        file_tokens = re.split("_", lite_file_basename)
+
+        product = file_tokens[1]
+        yymmdd = file_tokens[2]
+        version = file_tokens[3]
+
+        date = "20" + yymmdd[:2] + "-" + yymmdd[2:4] + "-" + yymmdd[4:]
+
+        plot_tags = yymmdd + "_" + version + ".png"
+        
+        if user_defined_var_list:
+            var_list = user_defined_var_list
         else:
-            loop_list = list(var_list)
-            for var in loop_list:
-                #print var
-                if glob(os.path.join(out_plot_dir, var + "_NE_" + global_plot_name_tags)) and glob(os.path.join(out_plot_dir, var + "_SE_" + global_plot_name_tags)) and glob(os.path.join(out_plot_dir, var + "_SW_" + global_plot_name_tags)) and glob(os.path.join(out_plot_dir, var + "_NW_" + global_plot_name_tags)):
-                    var_list.remove(var)
-        if not var_list:
-            print("All plots exist. To overwrite, change the value of 'overwrite' to True")
-            print("Exiting.")
-            sys.exit()
-    if verbose:
-        print("Variables to be plotted: " + str(var_list))
-        if overwrite:
-            print("Any existing plots for these variables in " + out_plot_dir + " will be overwritten")
-            print("This is the default behavior. To change, change the value of 'overwrite' to False")
+            var_list = data_dict[product].keys()  
+
+        for var in var_list:
+            if verbose:
+                print("Processing "+ var)
+            if var not in data_dict[product].keys():
+                print(var + " is not defined in the " + product + " data dictionary. Please add it or check spelling.")
+                print("Exiting.")
+                sys.exit()
+    
+        if not overwrite:
+            #double check there's something to do
+            if extent_box:
+                for var in var_list:
+                    out_plot_name = get_image_filename(out_plot_dir, var, extent_box, plot_tags)
+                    if glob(out_plot_name):
+                        var_list.remove(var)
+            else:
+                loop_list = list(var_list)
+                for var in loop_list:
+                    if verbose:
+                        print(v)
+                    for t in tile_dict.keys():
+                        if verbose:
+                            print(t)
+                        out_plot_name = get_image_filename(out_plot_dir, var, tile_dict[t]["extent_box"], plot_tags)
+                        if glob(out_plot_name):
+                            var_list.remove(var)
+            if not var_list:
+                print("All plots exist. To overwrite, change the value of 'overwrite' to True by setting the @w command line option")
+                print("Exiting.")
+                sys.exit()
+        
+        if verbose:
+            print("Variables to be plotted: " + str(var_list))
+            if overwrite:
+                print("Any existing plots for these variables in " + out_plot_dir + " will be overwritten")
+                print("To change this behavior, remove the @w command line option")
 
