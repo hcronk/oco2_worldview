e718547b1c13ecfb322de4fee29f7cdc888013b8
Heather Cronk <heather.cronk@colostate.edu>
2018-09-24 10:28:58 -0600

    Check for final RGB if RGB argument is set when deciding whether to process. Create and use a more specific intermediate tif name to accommodate multiple RGB pulls at the same time without stepping on each other.

--- code/jobbuilder/on_demand_processing.py
+++ code/jobbuilder/on_demand_processing.py
@@ -5,7 +5,7 @@ from glob import glob
 import operator
 import re
 import numpy as np
-from routine_processing import get_image_filename, check_processing_or_problem, build_config
+from routine_processing import get_image_filename, check_processing_or_problem, build_config, get_intermediate_tif_filename
 code_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)), os.path.pardir)
 sys.path.append(code_dir)
 from oco2_worldview_imagery import stitch_quadrants
@@ -122,7 +122,9 @@ if __name__ == "__main__":
                         }
 
     if rgb:
-        rgb = os.path.join(code_dir, "GIBS_Aqua_MODIS_truecolor.xml")
+        rgb = [os.path.join(code_dir, "GIBS_Aqua_MODIS_truecolor.xml")]
+    else:
+        rgb_list = False
 
     for lf in files:
         if verbose:
@@ -163,13 +165,17 @@ if __name__ == "__main__":
                 if verbose:
                     print("Checking " + var)
                 out_plot_name = get_image_filename(var, extent_box, plot_tags)
-                if debug or overwrite or not glob(out_plot_name):
+                layered_rgb_name = os.path.join(out_plot_dir, re.sub(var, var +"_onRGB", os.path.basename(out_plot_name)))
+                if rgb:
+                    intermediate_tif = [get_intermediate_tif_filename(extent_box, date)]
+                    rgb_list = rgb + intermediate_tif
+                if debug or overwrite or not glob(out_plot_name) or (rgb and not glob(layered_rgb_name)):
                     job_file = re.sub("png", "pkl", os.path.basename(out_plot_name))
                     processing_or_problem = check_processing_or_problem(job_file, verbose=verbose)
                     if not processing_or_problem:
                         if verbose:
                             print("Creating config file for " + var)
-                        build_config(lf, lite_file_substring_dict["product"], var, extent_box, out_plot_name, job_file, rgb=rgb, debug=debug, verbose=verbose)
+                        build_config(lf, lite_file_substring_dict["product"], var, extent_box, out_plot_name, job_file, rgb=rgb_list, debug=debug, verbose=verbose)
                 else:
                     if verbose:
                         print(out_plot_name + " exists and will not be overwritten.")
@@ -185,21 +191,21 @@ if __name__ == "__main__":
                     if verbose:
                         print("Checking " + t + " tile")
                     out_plot_name = get_image_filename(var, TILE_DICT[t]["extent_box"], plot_tags)
+                    layered_rgb_name = os.path.join(out_plot_dir, re.sub(var, var +"_onRGB", os.path.basename(out_plot_name)))
                     if stitch:
                         if rgb:
-                            out_plot_dir = os.path.dirname(out_plot_name)
-                            just_plot_name = os.path.basename(out_plot_name)
-                            layered_rgb_name = os.path.join(out_plot_dir, re.sub(var, var +"_onRGB", just_plot_name))
+                            intermediate_tif = [get_intermediate_tif_filename(TILE_DICT[t]["extent_box"], date)]
+                            rgb_list = rgb + intermediate_tif
                             plots_to_stitch[t] = layered_rgb_name
                         else:
                             plots_to_stitch[t] = out_plot_name
-                    if debug or overwrite or not glob(out_plot_name):
+                    if debug or overwrite or not glob(out_plot_name) or (rgb and not glob(layered_rgb_name)):
                         job_file = re.sub("png", "pkl", os.path.basename(out_plot_name))
                         processing_or_problem = check_processing_or_problem(job_file, verbose=verbose)
                         if not processing_or_problem:
                             if verbose:
                                 print("Creating config file for " + var)
-                            build_config(lf, lite_file_substring_dict["product"], var, TILE_DICT[t]["extent_box"], out_plot_name, job_file, rgb=rgb, debug=debug, verbose=verbose)
+                            build_config(lf, lite_file_substring_dict["product"], var, TILE_DICT[t]["extent_box"], out_plot_name, job_file, rgb=rgb_list, debug=debug, verbose=verbose)
                     else:
                         if verbose:
                             print(out_plot_name + " exists and will not be overwritten.")
@@ -210,5 +216,6 @@ if __name__ == "__main__":
                     absorb = [tokens.remove(token) for token in tokens if re.search("Lon", token)]
                     intermediate = ""
                     stitched_plot_name = "".join([intermediate + token + "_" if not re.search("Lat", token) else intermediate + "stitched_" for token in tokens])[:-1]
-                    success = stitch_quadrants(plots_to_stitch, os.path.join(out_plot_dir, stitched_plot_name))
+                    if debug or overwrite or not glob(stitched_plot_name):
+                        success = stitch_quadrants(plots_to_stitch, os.path.join(out_plot_dir, stitched_plot_name))
             
