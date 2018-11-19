66c2b09289c979621650271df9c33751c794125e
Heather Cronk <heather.cronk@colostate.edu>
2018-11-19 13:56:48 -0800

    Updates to produce SIF cmaps with data range [-1, 2] in 0.015 increments and TCWV cmap with data range [0,75] in 0.33/0.34 increments

--- code/utils/create_gibs_cmaps.py
+++ code/utils/create_gibs_cmaps.py
@@ -4,34 +4,37 @@ import matplotlib as mpl
 from matplotlib import cm
 import numpy as np
 import pandas as pd
-import operator
+import re
+import math
 
 CMAP_CSV_DIR = "/home/hcronk/oco2_worldview/code/utils/gibs_cmaps"
-NCOLORS=256
 
-DATA_DICT = { "LtCO2" : 
-                { "xco2" : {"data_field_name" : "xco2", "preprocessing" : False, "range": [380, 430], "cmap" : "viridis", "quality_info" : {"quality_field_name" : "xco2_quality_flag", "qc_val" :  0, "qc_operator" : operator.eq }}, 
-                  "xco2_relative" : {"data_field_name" : None, "preprocessing" : "ftp://aftp.cmdl.noaa.gov/products/trends/co2/co2_trend_gl.txt", "range": [-6, 6], "cmap" : "RdBu_r", "quality_info" : {"quality_field_name" : "xco2_quality_flag", "qc_val" :  0, "qc_operator" : operator.eq }}, 
-                  "tcwv" : {"data_field_name" : "Retrieval/tcwv", "preprocessing" : False, "range": [0, 75], "cmap" : "Blues", "quality_info" : {}}, 
-                },
-             "LtSIF" : 
-                { "sif757" : {"data_field_name" : "SIF_757nm", "preprocessing" : False, "range": [0, 2], "cmap" : "YlGn", "quality_info" : {}}, 
-                  "sif771" : {"data_field_name" : "SIF_771nm", "preprocessing" : False, "range": [0, 2], "cmap" : "YlGn", "quality_info" : {}}, 
-                  "sif_blended" : {"data_field_name" : None, "preprocessing" : True, "range": [0, 2], "cmap" : "YlGn", "quality_info" : {}}
-                }
+DATA_DICT = { "xco2" : {"range": [380, 430], "cmap" : "cm.viridis", "binsize": 0.2}, 
+              "xco2_relative" : {"range": [-6, 6], "cmap" : "cm.RdBu_r", "binsize": 0.05}, 
+              "tcwv" : {"range": [0, 75], "cmap" : "cm.Blues", "binsize": 1/3.}, 
+              "sif757" : {"data_field_name" : "SIF_757nm", "preprocessing" : False, "range": [-1, 2], "cmap" : "cm.YlGn", "binsize": 0.015}, 
+              "sif771" : {"data_field_name" : "SIF_771nm", "preprocessing" : False, "range": [-1, 2], "cmap" : "cm.YlGn", "binsize": 0.015}, 
+              "sif_blended" : {"data_field_name" : None, "preprocessing" : True, "range": [-1, 2], "cmap" : "cm.YlGn", "binsize": 0.015}
             }
 
 
-for key, v in DATA_DICT.items():
-    for var in v.keys():
+def truncate(n, d):
+    return math.floor(n * 10 ** d) / 10 ** d
 
-        cmap_name = os.path.join(CMAP_CSV_DIR, var + "_" + DATA_DICT[key][var]["cmap"] + "_" +  str(DATA_DICT[key][var]["range"][0]) + "to" + str(DATA_DICT[key][var]["range"][1]) + ".csv")
-        data_step = (DATA_DICT[key][var]["range"][1] - DATA_DICT[key][var]["range"][0])/float(NCOLORS)
+for var in DATA_DICT.keys():
+    #print(var)
+    cmap_name = os.path.join(CMAP_CSV_DIR, var + "_" + re.split("\.", DATA_DICT[var]["cmap"])[-1] + "_" +  str(DATA_DICT[var]["range"][0]) + "to" + str(DATA_DICT[var]["range"][1]) + ".csv")
+    
+    data_crange_low = [i*DATA_DICT[var]["binsize"] for i in np.arange(DATA_DICT[var]["range"][0] / DATA_DICT[var]["binsize"], DATA_DICT[var]["range"][1] / DATA_DICT[var]["binsize"] + DATA_DICT[var]["binsize"], 1)][:-1]
+    data_crange_high = [i*DATA_DICT[var]["binsize"] for i in np.arange(DATA_DICT[var]["range"][0] / DATA_DICT[var]["binsize"], DATA_DICT[var]["range"][1] / DATA_DICT[var]["binsize"] + DATA_DICT[var]["binsize"], 1)][1:]
+    
+    ncolors = len(data_crange_low)
 
-        data_crange_low = np.arange(DATA_DICT[key][var]["range"][0], DATA_DICT[key][var]["range"][1] + data_step, data_step, dtype=float)[:-1]
-        data_crange_high = np.arange(DATA_DICT[key][var]["range"][0], DATA_DICT[key][var]["range"][1] + data_step, data_step, dtype=float)[1:]
+    cmap_arr = np.array([[np.append(np.round(255*mpl.colors.to_rgba_array(eval(DATA_DICT[var]["cmap"])(n))), [round(data_crange_low[n], 3), round(data_crange_high[n], 3)])] for n in range(ncolors)]).squeeze()
+    cmap_df = pd.DataFrame(cmap_arr, columns = ["red", "green", "blue", "alpha", "data_lim_low", "data_lim_high"]).astype({"red": int, "green": int, "blue": int, "alpha": int})
+    
+    if var == "tcwv":
+        cmap_df["data_lim_low"] = cmap_df["data_lim_low"].map(lambda x: truncate(x, 2))
+        cmap_df["data_lim_high"] = cmap_df["data_lim_high"].map(lambda x: truncate(x, 2))
 
-        cmap_arr = np.array([[np.append(mpl.colors.to_rgba_array(cm.viridis(n)),[data_crange_low[n], data_crange_high[n]])] for n in range(NCOLORS)]).squeeze()
-        cmap_df = pd.DataFrame(cmap_arr, columns = ["red", "green", "blue", "alpha", "data_lim_low", "data_lim_high"])
-        
-        cmap_df.to_csv(cmap_name, index=False)
+    cmap_df.to_csv(cmap_name, index=False)
